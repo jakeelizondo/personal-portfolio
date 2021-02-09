6c38737e55a87d680513d693993091903c91a52c
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-09 17:57:12 -0500

    homepage verbiage changes

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -17,12 +17,11 @@ export default function () {
         <img src={profPic} alt="jake-elizondo-developer-pic" />
         <div className="intro-content">
           <p className="dark-text">
-            I am a digital-marketer-turned-software-developer based out of
-            Raleigh, North Carolina. My goal is to build intuitive tools that
-            solve problems, designed with the end-user experience at the
-            forefront. My digital marketing experience has made me passionate
-            about designing websites that provide seamless experiences across
-            devices and operating systems.
+            I am a digital-marketer-turned-software-developer based out of Palo
+            Alto, CA. I build intuitive tools that solve problems, designed with
+            the end-user experience at the forefront. My digital marketing
+            experience has made me passionate about designing websites that
+            provide seamless experiences across devices and operating systems.
           </p>
         </div>
       </section>
