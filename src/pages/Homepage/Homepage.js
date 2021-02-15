d6d45618190591758754fd2df234e496dfa27e3d
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-15 14:14:16 -0500

    color changes

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -8,23 +8,21 @@ import ContactMeFormDark from '../../components/UI/organisms/ContactMeFormDark/C
 export default function () {
   return (
     <React.Fragment>
-      <section className={['headline', 'dark-text'].join(' ')}>
-        <div>
-          <h1>Hey, I'm Jake.</h1>
-        </div>
-        <div>
-          <h2>Let's build something together.</h2>
-        </div>
-      </section>
       <section className="intro-section">
         <img src={profPic} alt="jake-elizondo-developer-pic" />
         <div className="intro-content">
+          <div className={['headline', 'dark-text'].join(' ')}>
+            <div>
+              <h1>Hey, I'm Jake.</h1>
+            </div>
+            <div>
+              <h2>Let's build something together.</h2>
+            </div>
+          </div>
           <p className="dark-text">
             I am a digital-marketer-turned-software-developer based out of Palo
             Alto, CA. I build intuitive tools that solve problems, designed with
-            the end-user experience at the forefront. My digital marketing
-            experience has made me passionate about designing websites that
-            provide seamless experiences across devices and operating systems.
+            the end-user experience at the forefront.
           </p>
         </div>
       </section>
