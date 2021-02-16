7927c35060643b6bf41b7a555b1c3bb9d11d4962
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-16 08:56:07 -0500

    switched order of project buttons

--- src/projects.js
+++ src/projects.js
@@ -6,6 +6,7 @@ const PROJECTS = [
       'Full stack application for creating, updating, and tagging gift ideas throughout the year. Designed and built front end application with React to integrate with Node.js server. Designed and built database and server to store and manage application users and data. Includes user registration and authentication system.',
     skills: ['React', 'NodeJS', 'PostgreSQL'],
     links: [
+      { name: 'Live App', url: 'https://gift-closet.vercel.app/' },
       {
         name: 'Client Github',
         url: 'https://github.com/jakeelizondo/gift-closet-client',
@@ -14,7 +15,6 @@ const PROJECTS = [
         name: 'Server Github',
         url: 'https://github.com/jakeelizondo/gift-closet-api',
       },
-      { name: 'Live App', url: 'https://gift-closet.vercel.app/' },
     ],
   },
   {
@@ -24,11 +24,11 @@ const PROJECTS = [
       'This application will be growing over time to become a place where people can store items and campaign information for their Dungeons & Dragons campaigns. For now, it is a simple item generator to give dungeon masters ideas about items to include. The items in the generator are contributed by fellow players.',
     skills: ['React', 'HTML5', 'CSS3'],
     links: [
+      { name: 'Live App', url: 'https://the-item-vault.vercel.app/' },
       {
         name: 'Client Github',
         url: 'https://github.com/jakeelizondo/the-item-vault',
       },
-      { name: 'Live App', url: 'https://the-item-vault.vercel.app/' },
     ],
   },
 ];
